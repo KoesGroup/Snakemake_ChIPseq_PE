7c048fe6064af477a73bfde90fca49f9bf7bfefc
Jihed Chouaref <jihed.chouaref@gmail.com>
2018-07-20 17:18:41 +0200

    Reduced -lwalltime
    On lisa it required approximately 25 minutes

--- bowtie2_build_index_tomato_lisa_180718.sh
+++ bowtie2_build_index_tomato_lisa_180718.sh
@@ -1,11 +1,10 @@
 #PBS -S /bin/bash
 #PBS -lnodes=1:ppn=16
-#PBS -lwalltime=100:00:00
+#PBS -lwalltime=0:45:00
 	# if the script does not finish in 8hrs, give more hours for calculations
 	# (up to 120:00:00 for 5 days)
 
 #bowtie2 version 2.2.4
-Jihed is dumb
 
 module load bowtie/2.2.4
 
@@ -13,7 +12,7 @@ module load bowtie/2.2.4
 cp "${HOME}"/genomes/S_lycopersicum_chromosomes.3.00.fa "${TMPDIR}"/
 
 #build index from the reference genome
-bowtie2-build S_lycopersicum_chromosomes.3.00.fa tomato_inx
+bowtie2-build S_lycopersicum_chromosomes.3.00.fa tomato_inx_3.00
 # bowtie-build [reference fasta] [base name for index file]
 
 mv "${TMPDIR}"/* "${HOME}"/genomes/
